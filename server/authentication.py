0a9ca0c89266039132e68d45dfa656bb458ba633
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-04-21 17:52:08 -0300

    Escondendo as chaves privadas

--- server/authentication.py
+++ server/authentication.py
@@ -3,8 +3,26 @@ from firebase_admin import credentials
 from firebase_admin import auth
 import json
 import requests
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("key.json")
+load_dotenv()
+
+credjson = {
+  "type": "service_account",
+  "project_id": "fallguys-c4e9b",
+  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+  "private_key": os.getenv('PRIVATE_KEY'),
+  "client_email": "firebase-adminsdk-k5lql@fallguys-c4e9b.iam.gserviceaccount.com",
+  "client_id": "117028498074544091262",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-k5lql%40fallguys-c4e9b.iam.gserviceaccount.com"
+}
+
+cred = credentials.Certificate(credjson)
 firebase_admin.initialize_app(cred)
 
 def login(email: str, password: str):
