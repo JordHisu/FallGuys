9b3d42191f57e813f0f6c05639ecd491269dcd79
JordHisu <jord_aqui@hotmail.com>
2022-05-01 23:19:42 -0300

    Completed footer layout and logic

---
+++ app/props/navbar_button.py
@@ -0,0 +1,16 @@
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+
+
+class NavbarButton(ButtonBehavior, Image):
+    def toggle(self):
+        self.source = self.unselected_image if self.selected else self.selected_image
+        self.selected = not self.selected
+
+    def unselect(self):
+        self.source = self.unselected_image
+        self.selected = False
+
+    def select(self):
+        self.source = self.selected_image
+        self.selected = True
