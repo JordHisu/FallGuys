4a4cf415c4e23ac03482b81274ccc73dc9d05316
csnoboa <caique2noboa@gmail.com>
2022-05-03 19:32:19 -0300

    add working lora2 - send and receive

--- embedded/files/tests/lora2/transmitter.py
+++ embedded/files/tests/lora2/transmitter.py
@@ -1,15 +1,23 @@
 import utime
 from machine import UART
+from files.utils.blink_led import blink_led
 
 # https://www.youtube.com/watch?v=BEAHCSFP2OQ
 # https://www.micropeta.com/video24
 
 def send(string):
     lora = UART(0, 9600)
-    for i in range(10):
-        bytes_sent = lora.write(string)
-        print("Bytes sent:", bytes_sent)
-        utime.sleep(0.5)
+    blink_led(0.2)
+    bytes_sent = lora.write(string)
+    print("Bytes sent:", bytes_sent)
+    utime.sleep(1)
+
+def receive():
+    lora = UART(0, 9600)
+    blink_led(0.2)
+    dataRead = lora.readline()
+    print(dataRead)
+    utime.sleep(0.2)
 
 
 if __name__ == "__main__":
