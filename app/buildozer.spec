93e6d2cde5310e9a4b10b37ecf0dea8182086299
JordHisu <jord_aqui@hotmail.com>
2022-05-21 19:56:22 -0300

    Final changes to app

--- app/buildozer.spec
+++ app/buildozer.spec
@@ -37,7 +37,7 @@ version = 0.7
 
 # (list) Application requirements
 # comma separated e.g. requirements = sqlite3,kivy
-requirements = python3,kivy,kivy_garden.mapview,openssl,requests,charset_normalizer,chardet,idna,urllib3,certifi
+requirements = python3,kivy,kivy_garden.mapview,openssl,requests,charset_normalizer,chardet,idna,urllib3,certifi,requests
 
 # (str) Custom source folders for requirements
 # Sets custom source for any requirements with recipes
--- app/utils/data/gps_data.py
+++ app/utils/data/gps_data.py
@@ -6,3 +6,21 @@ class GPSData:
     data_type: str
     lat: float
     lon: float
+
+    def __post_init__(self):
+        self.filter_lat()
+        self.filter_lon()
+
+    def filter_lat(self):
+        if self.lat >= 90:
+            self.lat = 89.9
+        elif self.lat <= -90:
+            self.lat = -89.9
+
+    def filter_lon(self):
+        if self.lon >= 180:
+            self.lon = 179.9
+        elif self.lon <= -180:
+            self.lon = -179.9
+
+
