127cef16af3e7f90abbc7f923aebea26826f9059
Pedro Francescon Cittolin <pfcittolin@gmail.com>
2022-05-25 23:01:07 -0300

    Update LoRa class

--- embedded/files/base.py
+++ embedded/files/base.py
@@ -1,4 +1,5 @@
 from files.modules.lora import LoRa
+from files.modules.gsm import GSM
 from files.utils.logger import Logger
 import utime
 
@@ -9,18 +10,36 @@ class Base:
             uart_num=0,
             tx_pin=0,
             rx_pin=1,
+            m0=6,
+            m1=7,
             log=self.log
         )
+        # self.gsm = GSM(
+        #     tx_pin=16,
+        #     rx_pin=17,
+        #     power_pin=22,
+        #     debug=False,
+        #     carrier='timbrasil.br',
+        #     carrier_usr='tim',
+        #     carrier_pwd='tim',
+        #     srv_endpoint='http://fall-guys-integration-workship.herokuapp.com',
+        #     srv_user='c'
+        # )
 
     def run(self):
         while True:
             rcv_msg = self.lora.receive()
             if rcv_msg:
-                self.log.info('Received message from LoRa '+ str(rcv_msg))
-                if 'F' in rcv_msg:
-                    print('FALL DETECTED')
-                    self.log.info('FALL DETECTED')
-                elif 'B' in rcv_msg:
-                    print('BUTTON PRESSED')
-                    self.log.info('BUTTON PRESSED')
-            utime.sleep(0.2)
+                print(rcv_msg)
+                # type = rcv_msg['type']
+                # if type == 'GPS':
+                #     self.gsm.send_data(None, [rcv_msg['lat'], rcv_msg['lon']], None)
+                # if type == 'ACC':
+                #     self.gsm.send_data(rcv_msg['steps'], None, None)
+                # if type == 'BAR':
+                #     print(rcv_msg)
+                #     # send A to server
+                # if type == 'BUT':
+                #     self.gsm.send_notification('panic')
+
+            utime.sleep(0.4)
