234ad4e994ca8501b5bb2c28f500bd168a5d2f1b
Pedro Francescon Cittolin <pfcittolin@gmail.com>
2022-06-01 00:17:03 -0300

    Reset main

--- embedded/main.py
+++ embedded/main.py
@@ -1,22 +1,15 @@
-# from files.modules.measurements import Measurements
+from files.modules.measurements import Measurements
 
-# from files.base import Base
-# base = Base()
-
-from files.anklet import Anklet
-anklet = Anklet()
 
 def run():
-    # base.run()
-    anklet.run()
-#     measurements = Measurements(stepcallback)
-#     measurements.start()
+    measurements = Measurements(stepcallback)
+    measurements.start()
 
-# steps = 0
-# def stepcallback():
-#     global steps
-#     steps += 1
-#     print(steps)
+steps = 0
+def stepcallback():
+    global steps
+    steps += 1
+    print(steps)
 
 if __name__ == "__main__":
     run()
