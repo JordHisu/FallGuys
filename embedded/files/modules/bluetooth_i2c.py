c735ba8bb42ccbdc82919eb17b3d85c945a59e4f
JordHisu <jord_aqui@hotmail.com>
2022-06-22 02:28:44 -0300

    final modifications to accelerometer

--- embedded/files/modules/bluetooth_i2c.py
+++ embedded/files/modules/bluetooth_i2c.py
@@ -1,29 +1,26 @@
-import machine
-import time
+from files.utils.smbus import SMBus
 
 
 class Bluetooth:
-    def __init__(self, i2c, log, address=0x50):
+    def __init__(self, i2c, i2c_lock, log, address=0x50):
         self.i2c = i2c
         self.address = address
         self.log = log
-
-        # mac = self.i2c.readfrom_mem(self.address, 0x11, 6)
-        # time.sleep(0.05)
-        # print('MODEL -> ' + str(mac))
-
-    # def send(self, msg):
-    #     print("Sending: " + str(msg))s
-    #     self.i2c.writeto_mem(self.address, 0xF0, msg)  # Master
+        self.smbus = SMBus(self.i2c, i2c_lock)
 
     def receive(self):
-        rcv_length = self.i2c.readfrom_mem(self.address, 0xF1, 1)
-        if rcv_length:
-            rcv_length = int.from_bytes(rcv_length, 'big')
-            if 0 < rcv_length < 255:
-                rcv = self.i2c.readfrom_mem(self.address, 0xF2, rcv_length)
-                if rcv:
-                    return rcv.decode()
-        return None
+        rcv_length = self.smbus.read_i2c_block_data(self.address, 0xF1, 1)
+        if not rcv_length:
+            return None
+
+        rcv_length = int.from_bytes(rcv_length, 'big')
+        if not(0 < rcv_length < 255):
+            return None
+
+        rcv = self.smbus.read_i2c_block_data(self.address, 0xF2, rcv_length)
+        if not rcv:
+            return None
+
+        return rcv.decode()
 
 
--- embedded/files/modules/measurements.py
+++ embedded/files/modules/measurements.py
@@ -1,28 +1,34 @@
 import gc
 from files.modules.accelerometer import Accelerometer
 from _thread import start_new_thread
-import utime
+from utime import sleep_ms
 
 
 class Measurements:
-    def __init__(self, stepcallback, i2c) -> None:
+    def __init__(self, stepcallback, i2c, i2c_lock) -> None:
         self.accelerometer = Accelerometer(
             i2c=i2c,
             log=None,
             address=0x53,
-            irq_callback=stepcallback
+            irq_callback=stepcallback,
+            i2c_lock=i2c_lock
         )
     
     def _loop(self):
         i = 0
-        while True:
-            i += 1
-            if i % 25 is 0:
-                gc.collect()
-            self.accelerometer.stepIteration()
+        try:
+            while True:
+                i += 1
+                if i % 25 is 0:
+                    gc.collect()
+                self.accelerometer.stepIteration()
+        except Exception as e:
+            gc.collect()
+            print(e)
     
     def start(self):
         start_new_thread(self._loop, ())
+
 
 if __name__ == "__main__":
     m = Measurements()
